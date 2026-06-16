ecea407aae1dd78957230f7c18d0b5dd6574f3d4
Emadsiddig <emadsiddig72@gmail.com>
2026-06-16 10:07:58 +0200

    Exersises js

---
+++ 01-fundamentals/jsCondidtionals/2.Sign_of_Product_of_Three_Numbers.js
@@ -0,0 +1,15 @@
+var num1=-2 ,num2=-2,num3=3;
+
+if(num1 > 0 && num2 > 0 && num3 > 0){
+ console.log("The sign is +");
+}else if(num1 < 0 && num2 < 0 && num3 < 0){
+    console.log("The sign is +");
+}else if(num1 > 0 && num2 < 0 && num3 < 0){
+      console.log("The sign is +");
+}else if(num1< 0 && num2 > 0 && num3 < 0){
+    console.log('the sign is +');
+}else if(num1< 0 && num2 < 0 && num3 > 0){
+    console.log('the sign is +');
+}else {
+    console.log('the sign is -');
+}
