543390318b95965e05b4a7f175e435090889d675
Emadsiddig <emadsiddig72@gmail.com>
2026-06-16 10:18:29 +0200

    Exercises

---
+++ 6.Student_Grades_Calculation.js
@@ -0,0 +1,15 @@
+var David = 80 ,Vinoth = 77,Divya=88,Ishitha = 95,THomas=68;
+
+var avarage = (David+Vinoth+Divya+Ishitha+THomas)/5;
+
+   if(avarage < 60){
+    console.log('F');
+   }else if(avarage < 70 && avarage >60){
+    console.log('D');
+   }else if(avarage < 80 && avarage >70){
+    console.log('C');
+   }else if(avarage < 90 && avarage >80){
+    console.log('B');
+   }else if(avarage < 100 && avarage > 90){
+    console.log('A');
+   }
