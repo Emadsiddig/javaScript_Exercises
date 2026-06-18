8e891b806f25d20f8e1f81ed9cb0774a0e49ce47
Emadsiddig <emadsiddig72@gmail.com>
2026-06-18 17:48:28 +0200

    covering amount to coins

---
+++ 01-fundamentals/functions/conver_Amount_To_coins.js
@@ -0,0 +1,19 @@
+function convAmountTOCoins(amount, coins){
+    coins.sort((a, b) => b - a);
+    let arrOfcoins = [];
+
+    for(let i = 0; i < coins.length; i++){
+        let currentCoin = coins[i]; // Grab the actual coin value 
+        
+        let fit = Math.floor(amount / currentCoin);
+        
+        for(let j = 0; j < fit; j++){
+            arrOfcoins.push(currentCoin); // Use () and push the coin, not the counter
+        }
+        
+        amount %= currentCoin; // Remainder of the actual coin
+    }
+    return arrOfcoins;
+}
+
+console.log(convAmountTOCoins(323, [23, 44, 2, 1]));
