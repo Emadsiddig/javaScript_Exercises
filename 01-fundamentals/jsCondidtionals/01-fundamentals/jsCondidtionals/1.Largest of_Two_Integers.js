821bb028476614d805fa3053b192d8abf0650208
Emadsiddig <emadsiddig72@gmail.com>
2026-06-18 07:27:30 +0200

    delete Exercisess

--- 01-fundamentals/jsCondidtionals/01-fundamentals/jsCondidtionals/1.Largest of_Two_Integers.js
+++
@@ -1,9 +0,0 @@
-let a = 12;
-let b =22;
-if(a>b){
-    console.log(a);
-}else if(b>a){
-    console.log(b);
-}else{
-    console.log("the numbers ara equle");
-}
--- 01-fundamentals/jsCondidtionals/01-fundamentals/jsCondidtionals/10.Pyramid_Patter_with_Nested_Loop.js
+++
@@ -1,8 +0,0 @@
-let star='';
-
-for(let i =1 ;i<=6;i++){
-  for(let j =1;j<i;j++){
-   star += '*';
-  }
-  console.log(star);
-}
